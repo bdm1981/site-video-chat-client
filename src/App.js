d0ce05d5fc77fc42170f1d02e419945f0ada9cff
Brad McAllister <bdm@bdmcomputers.com>
2020-09-03 12:17:50 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import VideoComp from "./components/VideoComp";
+
+function App() {
+  return (
+    <div className="App">
+      <VideoComp />
+    </div>
+  );
+}
+
+export default App;
